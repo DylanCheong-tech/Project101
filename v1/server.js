b465b47b74aafb044991b2cccf0f1a07340b95b5
DylanCheong-tech <74398592+DylanCheong-tech@users.noreply.github.com>
2021-10-18 16:12:38 +0800

    Twenty-fifth commit (18th October 2021)
    New Items Added:
    1. "static" directory is created for holding all the html files
    
    Progressions:
    1. index.html, productPage.html files are removed from the root directory due to the security issues
    2. server.js --> Node Express.js implementing routing for all the required web resources
    
    -- server code routing helps preventing user get access to resources by editing the URL intentionally

--- v1/server.js
+++ v1/server.js
@@ -2,6 +2,117 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, '/')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+// serve all the html files from the static directory 
+app.use(express.static(path.join(__dirname,"/static/")));
+
+// routing for css stylesheet files 
+app.get("/css/:stylesheet", (req, res, next) => {
+  var filename = req.params.stylesheet;
+  
+  console.log("Requesting : " + filename);
+
+  res.sendFile(path.join(__dirname + "/css/" + filename), (err) => {
+    if (err) {
+      res.sendStatus(404);
+      next(err);
+    }
+    else {
+      console.log(filename + " sent successfully !");
+    }
+  });
+});
+
+// routing for jsvascript files 
+app.get("/js/:scripts" , (req, res, next) => {
+  var filename = req.params.scripts;
+
+  console.log("Requesting : " + filename);
+
+  res.sendFile(path.join(__dirname , "/js/" + filename), (err) => {
+    if (err) {
+      res.sendStatus(404);
+      next(err);
+    }
+    else {
+      console.log(filename + " sent successfully !");
+    }
+  });
+});
+
+// routing for json data file 
+app.get("/json/:data" , (req, res, next) => {
+  var filename = req.params.data;
+  
+  console.log("Requesting : " + filename);
+
+  res.sendFile(path.join(__dirname , "/json/" + filename), (err) => {
+    if (err) {
+      res.sendStatus(404);
+      next(err);
+    }
+    else {
+      console.log(filename + " sent successfully !");
+    }
+  });
+})
+
+// routing for webpage images (icons, logos) 
+app.get("/img/:images", (req, res, next) => {
+  var filename = req.params.images;
+
+  console.log("Requesting : " + filename);
+
+  res.sendFile(path.join(__dirname , "/img/" + filename), (err) => {
+    if (err) {
+      res.sendStatus(404);
+      next(err);
+    }
+    else {
+      console.log(filename + " sent successfully !");
+    }
+  });
+});
+
+// routing for product category images 
+app.get("/img/:category/:subcategory/:images", (req, res, next) => {
+  var category = req.params.category;
+  var subcategory = req.params.subcategory;
+  var filename = req.params.images;
+
+  console.log("Requesting : " + category + "/" + filename);
+
+  res.sendFile(path.join(__dirname , "/img/" + category + "/" + subcategory + "/" + filename), (err) => {
+    if (err) {
+      res.sendStatus(404);
+      next(err);
+    }
+    else {
+      console.log(category + "/" + filename + " sent successfully !");
+    }
+  });
+});
+
+// routing for saucer product with text images sub-sub  directory
+app.get("/img/:category/:subcategory/:with_text/:images", (req, res, next) => {
+  var category = req.params.category;
+  var subcategory = req.params.subcategory;
+  var with_text = req.params.with_text;
+  var filename = req.params.images;
+
+  console.log("Requesting : " + category + "/" + filename);
+
+  res.sendFile(path.join(__dirname , "/img/" + category + "/" + subcategory + "/" +  with_text + "/" + filename), (err) => {
+    if (err) {
+      res.sendStatus(404);
+      next(err);
+    }
+    else {
+      console.log(category + "/" + filename + " sent successfully !");
+    }
+  });
+});
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
