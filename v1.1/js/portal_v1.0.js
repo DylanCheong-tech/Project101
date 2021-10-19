c3ad4d521a2213ae2d5ee5e476ac8630be27921a
DylanCheong-tech <74398592+DylanCheong-tech@users.noreply.github.com>
2021-10-19 21:20:08 +0800

    V1.1, Commit 3 (19th October 2021)
    Progressions :
    1. Validation when add new product, no repeat product code, the add button will be disabled when all input box is empty
    2. new added item will be appear at the first row
    2. Implementing filter (name) and sort (name, code) function
    3. Delete item function implemented
    4. the main data storage, this.products change to array format (value inside the object changed will not be detected by the Vue model)

--- v1.1/js/portal_v1.0.js
+++ v1.1/js/portal_v1.0.js
@@ -1,12 +1,21 @@
 var portal = new Vue ({
     el : "#content",
     data : {
-        products : {},
+        products : [],
         numbering: 1,
         addOperation : false,
         editOperation : false,
         deleteOperation : false,
-        productEditStatus : {}
+        productEditStatus : {},
+        newProduct : {
+            code : "",
+            name : "",
+            colors : "",
+            descriptions : "",
+            details : ""
+        },
+        // refresh : true,
+        filterKeyword : ""
     },
 
     methods:{
@@ -18,6 +27,28 @@ var portal = new Vue ({
         },
         deleteProduct () {
             this.deleteOperation = this.deleteOperation ? false : true;
+        },
+
+        addNewProduct () {
+            var newObj = {
+                "data" : {
+                    "color" : this.newProduct.colors,
+                    "details" : this.newProduct.details,
+                    "name" : this.newProduct.name,
+                    "code" : this.newProduct.code.toUpperCase()
+                }
+                
+            };
+
+            if (!this.codeExist)
+            {
+                this.products.unshift(newObj);
+            }
+
+            for (property in this.newProduct){
+                this.newProduct[property] = "";
+            }
+
         },
 
         productEditFired (code) {
@@ -36,8 +67,112 @@ var portal = new Vue ({
             this.editOperation = true;
 
             // logic implements here 
-        }
+        },
 
+        deleteExistingProduct () {
+            var checkboxes = $("input[name='delete']");
+            var deleteList = [];
+            
+            for (i = 0 ; i < checkboxes.length; i ++){
+                if (checkboxes.eq(i).prop("checked"))
+                {
+                    deleteList.push(checkboxes.eq(i).attr("value"));
+                }
+            }
+
+            function findIndex (code, products) {
+                for (index in products){
+                    if (products[index].data.code == code)
+                    {
+                        return index;
+                    }
+                }
+            }
+
+            alert("You are deleting : \n" + deleteList.join(", "));
+
+            // remove from the products array 
+            for (index in deleteList)
+            {
+                var removeIndex = findIndex(deleteList[index], this.products);
+                this.products.splice(removeIndex, 1);
+            }
+            
+            // unchecked the previous operation 
+            $("input[name='delete']").prop("checked", false);
+        },
+
+        // triggerRefresh () {
+        //     this.refresh = false;
+        //     setInterval(() => {
+        //         this.refresh = true;
+        //     }, 3000);
+        // },
+
+        sortProducts (event) {
+            // parseInt ignore the characters in the string 
+            var seq = parseInt(event.target.value);
+            
+            var mode = event.target.value.substr(-1);
+
+            function sort (mode) {
+                return function (a, b){
+                    if (a.data[mode].toLowerCase() < b.data[mode].toLowerCase())
+                    return -1 * seq;
+                
+                if (a.data[mode].toLowerCase() > b.data[mode].toLowerCase())
+                    return 1 * seq;
+
+                return 0;
+                }
+            }
+
+            switch (mode){
+                case "n" : this.products.sort(sort("name")); break;
+                case "c" : this.products.sort(sort("code")); break;
+                default : mode; // do nothing 
+            }
+            
+        },
+    },
+
+    computed : {
+        isEmpty () {
+            for (property in this.newProduct){
+                if (this.newProduct[property])
+                {
+                    return false;
+                }
+            }
+            return true;
+        },
+        products_arr () {
+            if (this.filterKeyword == "")
+                return this.products;
+
+            var tempArr = [];
+            for (index in this.products){
+                if (this.products[index].data.name.toLowerCase().includes(this.filterKeyword.toLowerCase())){
+                    tempArr.push(this.products[index]);
+                }
+            }
+            return tempArr;
+        },
+        noListing () {
+            return this.products_arr.length ? false : true;
+        },
+         
+        codeExist () {
+            var exist = false;
+            for (index in this.products)
+            {
+                if (this.products[index].data.code == this.newProduct.code.toUpperCase()){
+                    exist = true;
+                    break;
+                }
+            }
+            return exist;
+        }
     },
 
     created : function () {
@@ -66,8 +201,12 @@ var portal = new Vue ({
 
             this.products[product].color = tempArr.join(", ");
 
+            this.products[product].code = product;
+
             this.productEditStatus[product] = false;
         }
+
+        this.products = Object.entries(this.products).map((e) => ( { "data": e[1] } ));
 
         console.log(this.products);
     }
